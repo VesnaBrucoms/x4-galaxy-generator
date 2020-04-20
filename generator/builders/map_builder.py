b6f6ca8d9135920f1779bcf22db650a27078a973
Trevalyan <etstevens60@gmail.com>
2020-04-20 21:05:29 +0100

    MapBuilder refactoring to reduce repitition

--- generator/builders/map_builder.py
+++ generator/builders/map_builder.py
@@ -36,39 +36,19 @@ class MapBuilder:
             )
             create_sub_element(connection, "macro", connection="destination", path=dest)
 
-    def _add_galaxy_cluster(self, parent_element, cluster):
-        x_position, y_position = self._calculate_absolute_position(cluster.x, cluster.z)
-        connection = create_sub_element(
-            parent_element, "connection", name=cluster.connection_ref, ref="clusters"
-        )
-        offset = create_sub_element(connection, "offset")
-        create_sub_element(offset, "position", x=x_position, y="0", z=y_position)
-        create_sub_element(
-            connection, "macro", ref=cluster.macro_ref, connection="galaxy"
-        )
-
-    def _calculate_absolute_position(self, x_coord, z_coord):
-        x_absolute = x_coord * X_HEX_SPACING
-        z_absolute = z_coord * Z_HEX_SPACING
-        if x_coord % 2 != 0:
-            z_absolute += Z_HALF_SPACING
-
-        return (x_absolute, z_absolute)
-
     def build_cluster(self, cluster):
         macro = create_sub_element(
             self.clusters_root, "macro", name=cluster.macro_ref, class_attr="cluster"
         )
-
         create_sub_element(macro, "component", ref="standardcluster")
-
         connections = create_sub_element(macro, "connections")
         for sector in cluster.sectors:
-            connection = create_sub_element(
-                connections, "connection", name=sector.connection_ref, ref="sectors"
-            )
-            create_sub_element(
-                connection, "macro", ref=sector.macro_ref, connection="cluster"
+            self._add_connection(
+                connections,
+                sector.connection_ref,
+                "sectors",
+                sector.macro_ref,
+                "cluster",
             )
 
         if cluster.environment:
@@ -82,50 +62,38 @@ class MapBuilder:
         macro = create_sub_element(
             self.sectors_root, "macro", name=sector.macro_ref, class_attr="sector"
         )
-
         create_sub_element(macro, "component", ref="standardsector")
-
         connections = create_sub_element(macro, "connections")
         for zone in sector.zones:
-            connection = create_sub_element(
-                connections, "connection", name=zone.connection_ref, ref="zones"
-            )
-            create_sub_element(
-                connection, "macro", ref=zone.macro_ref, connection="sector"
+            self._add_connection(
+                connections, zone.connection_ref, "zones", zone.macro_ref, "sector"
             )
 
     def build_zone(self, zone):
         macro = create_sub_element(
             self.zones_root, "macro", name=zone.macro_ref, class_attr="zone"
         )
-
         create_sub_element(macro, "component", ref="standardzone")
-
         connections = create_sub_element(macro, "connections")
         for zone_object in zone.objects:
-            connection = create_sub_element(
+            position = {
+                "x": zone_object["x"],
+                "y": zone_object["y"],
+                "z": zone_object["z"],
+            }
+            rotation = {
+                "yaw": zone_object["yaw"],
+                "pitch": zone_object["pitch"],
+                "roll": zone_object["roll"],
+            }
+            self._add_connection(
                 connections,
-                "connection",
-                name=zone_object["name"],
-                ref=zone_object["type"],
-            )
-            offset = create_sub_element(connection, "offset")
-            create_sub_element(
-                offset,
-                "position",
-                x=zone_object["x"],
-                y=zone_object["y"],
-                z=zone_object["z"],
-            )
-            create_sub_element(
-                offset,
-                "rotation",
-                yaw=zone_object["yaw"],
-                pitch=zone_object["pitch"],
-                roll=zone_object["roll"],
-            )
-            create_sub_element(
-                connection, "macro", ref=zone_object["prop"], connection="space"
+                zone_object["name"],
+                zone_object["type"],
+                zone_object["prop"],
+                "space",
+                pos=position,
+                rot=rotation,
             )
 
     def get_result(self):
@@ -135,3 +103,54 @@ class MapBuilder:
             ElementTree.tostring(self.sectors_root),
             ElementTree.tostring(self.zones_root),
         )
+
+    def _add_galaxy_cluster(self, parent_element, cluster):
+        x_position, y_position = self._calculate_absolute_position(cluster.x, cluster.z)
+        position = {
+            "x": x_position,
+            "y": "0",
+            "z": y_position,
+        }
+        self._add_connection(
+            parent_element,
+            cluster.connection_ref,
+            "clusters",
+            cluster.macro_ref,
+            "galaxy",
+            pos=position,
+        )
+
+    def _add_connection(
+        self, parent_element, name, ref, macro_name, connection_ref, pos=None, rot=None,
+    ):
+        connection = create_sub_element(
+            parent_element, "connection", name=name, ref=ref
+        )
+        if pos or rot:
+            self._add_offset(connection, position=pos, rotation=rot)
+        create_sub_element(
+            connection, "macro", ref=macro_name, connection=connection_ref
+        )
+
+    def _calculate_absolute_position(self, x_coord, z_coord):
+        x_absolute = x_coord * X_HEX_SPACING
+        z_absolute = z_coord * Z_HEX_SPACING
+        if x_coord % 2 != 0:
+            z_absolute += Z_HALF_SPACING
+
+        return (x_absolute, z_absolute)
+
+    def _add_offset(self, parent_element, position=None, rotation=None):
+        offset = create_sub_element(parent_element, "offset")
+        if position:
+            create_sub_element(
+                offset, "position", x=position["x"], y=position["y"], z=position["z"],
+            )
+        if rotation:
+            create_sub_element(
+                offset,
+                "rotation",
+                yaw=rotation["yaw"],
+                pitch=rotation["pitch"],
+                roll=rotation["roll"],
+            )
